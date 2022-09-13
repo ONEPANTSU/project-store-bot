827aef9e0e561a99b2dcaac27fae1fec8c1d721a
ONEPANTSU <conandet@mail.ru>
2022-09-13 22:01:48 +0300

    init: connect to bot

--- project_store_bot.py
+++ project_store_bot.py
@@ -1,1 +1,10 @@
+import telebot
+import config
 
+bot = telebot.TeleBot(config.TOKEN)
+
+@bot.message_handler(content_types=['text'])
+def to_send(message):
+    bot.send_message(message.chat.id, message.text)
+
+bot.polling(non_stop=True)
