5ec8ac6cad7ec2351ce741c9536dbe5d7167d093
nikramiar <nikramiar@gmail.com>
2022-09-28 17:38:59 +0300

    feat: insert to DB from telegram

--- messages.py
+++ messages.py
@@ -3,7 +3,15 @@ sell_menu_message = "🗄 Мои предложения 🗄"
 
 put_up_for_sale_massage = "🖊Заполните анкету🖊"
 project_name_question = "Напишите название вашего проекта:"
+price_question = "Напишите цену вашего проекта"
+price_check_question = "Цена должна быть числом. Напишите цену вашего проекта:"
+subscribers_question = "Сколько подписчиков у вашего проекта?"
+subscribers_check_question = "Укажите количество подписчиков числом. Напишите сколько подписчиков у вашего проекта:"
+income_question = "Какой доход у вашего проекта?"
+income_check_question = "Укажите доход проекта числом. Какой доход у вашего проекта:"
 themes_question = "Выберете не более 3-х тематик вашего проекта:"
+comment_question = "Добавьте комментарий к обьявлению:"
+save_project_question = "Ваше обьявление сохранено!"
 
 get_list_of_projects_message = "🗄 Выставленные на продажу предложения 🗄"
 main_menu_massage = "📌 Главное меню 📌"
@@ -16,8 +24,16 @@ MESSAGES = {
     'start': start_message,
     'sell_menu': sell_menu_message,
     'put_up_for_sale': put_up_for_sale_massage,
-    'project_name_question': project_name_question,
-    'themes_question': themes_question,
+    'project_name': project_name_question,
+    'price': price_question,
+    'price_check': price_check_question,
+    'subscribers': subscribers_question,
+    'subscribers_check': subscribers_check_question,
+    'themes': themes_question,
+    'income': income_question,
+    'income_check': income_check_question,
+    'comment': comment_question,
+    'save_project': save_project_question,
     'get_list_of_projects': get_list_of_projects_message,
     'main_menu': main_menu_massage,
     'buy_menu': buy_menu_message,
